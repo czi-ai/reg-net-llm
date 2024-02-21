5236a0027eb1aebf0fd4acdd1edd990e069f3202
vinay-swamy <swamyvinny@gmail.com>
2024-02-21 11:45:00 -0500

    remove model.py

--- scGraphLLM/model.py
+++ scGraphLLM/model.py
@@ -1,1 +1,11 @@
 # For the model class
+import torch 
+class LitSCGraphLLM(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.graph_encoder = ...
+        self.transformer_encoder = ...
+    def graph_encoder_forward(self, x):
+        return self.graph_encoder(x)
+    def transformer_encoder_forward(self, x):
+        return self.transformer_encoder(x)
