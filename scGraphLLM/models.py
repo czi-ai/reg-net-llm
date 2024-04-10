3b1851bac74d61b594a039b9fe6620c7d19e790d
Vinay Swamy <vss2134@dbmis-mbp.dyn.nyp.org>
2024-04-10 16:01:17 -0400

    added more documentation

--- scGraphLLM/models.py
+++ scGraphLLM/models.py
@@ -7,7 +7,7 @@ from MLP_modules import *
 from transformer_modules import FlashTransformerEncoderLayer
 import lightning.pytorch as pl
 from data import pad_make_masks
-from _globals import *
+from _globals import * ## these define the indices for the special tokens 
 
 class LitScGraphLLM(pl.LightningModule):
     def __init__(self, config):
@@ -20,23 +20,41 @@ class LitScGraphLLM(pl.LightningModule):
         self.optim_config = config.optim_config
         self.loss_config = config.loss_config
     def forward(self, batch ):
+        ## adding in more explicit annotations of shapes and workflow
+        ## some definitions: 
+        ## G = the total number of unique genes across the whole dataset; g = the number of unique genes in a batch ; n = the number of cells in a batch; 
+        ## R= the total number of unique ranks across the whole dataset; r = the number of genes expressed in the cell with the most expressed genes, in this batch; the cells that have fewer expressed genes will have 0 padding so that the tensors are the same size
+        ## G_dim = the dimension of the gene embeddings; R_dim = the dimension of the node embeddings; L_dim  = the dimension of the LLM embeddings equalto G_dim + R_dim
+        ## e= the number of edges in a batch
         node_indices, edge_list,edge_weights, rank_data = batch
-        node_embeddings = self.node_embedding(node_indices)
+        ## Shapes: node_indices: gx1, ; edge_list: 2xe; edge_weights: e; 
+        node_embeddings = self.node_embedding(node_indices) ## maps g x 1 to g x G_dim
+        
         ## wasn't getting the gnn to work so jus commented it out for now
         ## but needs to basically take in node embeddings with shape nodes x edim and return the same sized, updated node embeddings
-        #node_embeddings, edge_index, edge_weight, attn_weight = self.gnn_encoder(node_embeddings, edge_list, edge_weights)
-        ranks, rank_global_gene_indices, rank_local_gene_indices = rank_data # both are nested tensors
-        rank_embeddings= pad_make_masks([self.rank_embedding(ranks[i]) for i in range(ranks.size(0))], return_mask=False)
+        #node_embeddings, edge_index, edge_weight, attn_weight = self.gnn_encoder(node_embeddings, edge_list, edge_weights) ## no shape changes, just updates inputs.
+
+        ranks, rank_global_gene_indices, rank_local_gene_indices = rank_data # 
+        # these are all NestedTensors(https://pytorch.org/docs/stable/nested.html) with integer valued rows;  Its analgous to a list of different length lists.
+        # ranks is the integer rank of an expressed gene in a cell; both rank_global_gene_indices and rank_local_gene_indices map gene identity to an integet value; each row is a cell with a different number of expressed genes, and we have n rows. 
+        ## the reason we have both rank_global_gene_indices and rank_local_gene_indices is because we need the rank_local_gene_indices to get the gene embeddings outputed by the gnn/initial node embeddings; we need the rank_global_gene_indices so that we know the identity of the gene for the MLM tasks
+
+        ##TODO: This can be refactored to not use a for loop, if we pad the rank indices before hand
+        rank_embeddings= pad_make_masks([self.rank_embedding(ranks[i]) for i in range(ranks.size(0))], return_mask=False) ## this individually maps each row in the NestedTensor to embeddings from the rank embedding layer, then pads then an concatenates them to a tensor of shape n x r x R_dim
+        
+        ##TODO: This should be able to be refactored as an indexing operation. To accomplish this,we'll need to append the <pad> and <mask> token values to the node_embedding tensors. This will letus use rank_global_gene_indices to index into the node_embeddings tensor. 
         gene_embeddings, attn_mask = pad_make_masks([node_embeddings[rank_local_gene_indices[i]] for i in range(rank_local_gene_indices.size(0))])
+        ## here, the individual integers in each row of rank_local_gene_indices are mapped to the embedding stored at that position in node_embeddings. We do a similar looping operation as above, but we also return an attention mask that corresponds to the padding. This function outputs an n x r x G_dim tensor and an n x r attention mask tensor
         global_gene_indices = pad_make_masks([rank_global_gene_indices[i] for i in range(rank_global_gene_indices.size(0))],  return_mask=False)
-        ### NOTE: this masking setup will allow padded tokens to be included in the masked language modeling task
-        ### we don't want to include this, so we will eventually need to refactor
-        masked_full_cell_embedding, mask_locs = self.mask_tensor(torch.cat([gene_embeddings, rank_embeddings], dim=2))
-        learned_cell_embedding = self.mlm_encoder(masked_full_cell_embedding, attn_mask)
+        
+
+        ### TODO: this masking setup will allow padded tokens to be included in the masked language modeling task. In order to fix this,We'll need to add the masking to the batch generation step in the dataloader. We also should also roughly standardize the tokens in each batch, or include a scaling factor for the loss based on the number of tokens in each batch 
+        masked_full_cell_embedding, mask_locs = self.mask_tensor(torch.cat([gene_embeddings, rank_embeddings], dim=2)) ## the rank embedding and gene embedding layers are concatenated together and masked. in the masking operation, 15% of the token embedding vectors are replaced with <MASK> values, which is a concatenation of the <MASK> token from the node_embedding layer and the <MASK> token from the rank_embedding layer. This is a n x r x L_dim tensor
+        learned_cell_embedding = self.mlm_encoder(masked_full_cell_embedding, attn_mask) ## this outputs an n x r x L_dim tensor
         return learned_cell_embedding, global_gene_indices, mask_locs
     def training_step(self, batch, batch_idx):
         learned_cell_embedding, rank_global_gene_indices, mask_locs = self(batch)
-        predicted_gene_id= self.prediction_head(learned_cell_embedding)
+        predicted_gene_id= self.prediction_head(learned_cell_embedding) ## this maps the n x r x L_dim tensor to an n x r x G tensor
         loss = self.mlm_loss(predicted_gene_id, rank_global_gene_indices, mask_locs)
         self.log('train_loss', loss)
         return loss
@@ -59,7 +77,8 @@ class LitScGraphLLM(pl.LightningModule):
         return masked_tensor, (batch_indices, seq_indices)
     def mlm_loss(self, predicted_gene_id, rank_global_gene_indices, mask_locs):
         batch_indices, seq_indices=mask_locs
-        masked_predictions = predicted_gene_id[batch_indices, seq_indices, :]
+
+        masked_predictions = predicted_gene_id[batch_indices, seq_indices, :] ## because we record the location of the masked tokens, we cna retrieve just the masked tokens, and collapse the first dimension, ie mapping from n x r x G to m x G where m is the number of masked tokens
         labels = rank_global_gene_indices[batch_indices, seq_indices]
         loss = F.cross_entropy(masked_predictions,labels)
         return loss
