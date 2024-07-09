Commented out positional embedding - does not scale well to graph size; fixed some bugs related to masking code. Model runs end to end.

scGraphLLM/GNN_modules.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch_geometric
 from torch_geometric.nn import GATConv
 from MLP_modules import AttentionMLP
 from torch_geometric.utils import to_dense_adj, dense_to_sparse, get_laplacian, add_self_loops
@@ -21,10 +22,9 @@ class GNN(nn.Module):
     self.out_dim = out_dim
     self.f = activation
     self.dropout = nn.Dropout(dropout_rate)
-    self.PE = SPosPE(projection_dim=out_dim) # assume out dim = in dim. Project from num_nodes dim
 
     # GAT layers
-    self.layers = nn.ModuleList([GATConv(input_dim + out_dim, hidden_dims[0], heads=num_heads[0], 
+    self.layers = nn.ModuleList([GATConv(input_dim, hidden_dims[0], heads=num_heads[0], 
                                          concat=False, dropout=dropout_rate)]) # Because of PE 
     for i in range(1, len(hidden_dims)):
       self.layers.append(GATConv(hidden_dims[i - 1], hidden_dims[i], heads=num_heads[i], 
@@ -46,8 +46,8 @@ class GNN(nn.Module):
 
   def forward(self, X, edge_index, edge_weight):
     # positional embedding based on shortest path distance
-    pos_embedding = self.PE(edge_index, X.shape[0])
-    X = torch.cat([X, pos_embedding], dim=1)
+    # pos_embedding = self.PE(edge_index, X.shape[0])
+    # X = torch.cat([X, pos_embedding], dim=1)
 
     # Virutal node connected to every other nodes
     virtual_node = torch.mean(X, dim=0, keepdim=True)

scGraphLLM/data.py
@@ -25,11 +25,9 @@ def transform_and_cache_aracane_graph_ranks( aracne_outdirs : List[str], gene_to
         network = pd.read_csv(aracne_out +"/aracne/consolidated-net_defaultid.tsv", sep = "\t")
         network_genes = list(set(network["regulator.values"].to_list() + network["target.values"].to_list()))
         ranks = pd.read_csv(aracne_out + "/rank_raw.csv") + 2 # keep only genes in the network, and offset the ranks by 2 to account for the special tokens, so 2 now corresponds to rank 0(ZERO_IDX)
-        print(ranks.shape)
         common_genes = list(set(network_genes).intersection(set(ranks.columns)))
 
         ranks = ranks.loc[:,common_genes]
-        print(ranks.shape)
         for i in range(ranks.shape[0]):
             if ncells % 1000 ==0:
                 print(f"Processed {ncells} cells", end="\r")
@@ -92,6 +90,9 @@ class AracneGraphWithRanksDataset(Dataset):
         ## mask 5% as a gene only mask; mask 5% as a rank only mask ; mask 5% as both gene and rank mask
         data = torch.load(self.cached_files[idx])
         node_indices = data.x
+        # need the clones otherwise the original indices will be modified
+        orig_gene_indices = node_indices[:, 0].clone()
+        orig_rank_indices = node_indices[:, 1].clone()
         ## for each mask type, create boolean mask of the same shape as node_indices
         gene_mask = torch.rand(node_indices.shape[0]) < mask_fraction
         rank_mask = torch.rand(node_indices.shape[0]) < mask_fraction
@@ -99,7 +100,7 @@ class AracneGraphWithRanksDataset(Dataset):
         node_indices[gene_mask, 0] = MASK_IDX
         node_indices[rank_mask, 1] = MASK_IDX
         node_indices[both_mask, :] = torch.tensor([MASK_IDX, MASK_IDX], dtype=node_indices.dtype)
-        return Data(x = node_indices, edge_index = data.edge_index, edge_weight = data.edge_weight, gene_mask = gene_mask, rank_mask = rank_mask, both_mask = both_mask, dataset_name = self.dataset_name)
+        return Data(x = node_indices, edge_index = data.edge_index, edge_weight = data.edge_weight, gene_mask = gene_mask, rank_mask = rank_mask, both_mask = both_mask, orig_gene_id = orig_gene_indices, orig_rank_indices = orig_rank_indices, dataset_name = self.dataset_name)
 
 class LitDataModule(pl.LightningDataModule):
     def __init__(self, data_config):

scGraphLLM/models.py
@@ -30,17 +30,16 @@ class LitScGraphLLM(pl.LightningModule):
         ## L_dim = the dimension of the learned cell embeddings(r_dim + g_dim);
         ## e= the number of edges in a batch
         node_indices, edge_list,edge_weights = batch.x, batch.edge_index, batch.edge_weight
-        mask_locs = [batch.gene_mask_locs, batch.rank_mask_locs, batch.both_mask_locs]
+        mask_locs = [batch.gene_mask, batch.rank_mask, batch.both_mask]
         node_indices = node_indices.type(torch.long)
         ## Shapes: node_indices: n x g x 2, ; edge_list: 2xe; edge_weights: e; 
         node_embeddings = self.node_embedding(node_indices).flatten(1) ## maps n x g x 
         
         ## take in node embeddings with shape nodes x edim and return the same sized, updated node embeddings
         node_embeddings = self.gnn_encoder(node_embeddings, edge_list, edge_weights) ## no shape changes, just updates inputs.
-        gnn_out = node_embeddings
 
-        gene_ids = node_indices[ :, 0] ## n x r
-        rank_ids = node_indices[ :, 1] 
+        gene_ids = batch.orig_gene_id
+        rank_ids = batch.orig_rank_indices
         return node_embeddings,edge_list, gene_ids, rank_ids, mask_locs
     
     def _step(self, batch, batch_idx):
@@ -57,44 +56,28 @@ class LitScGraphLLM(pl.LightningModule):
         L_mlm_rankboth = self.mlm_loss(predicted_rank_id, target_rank_ids, both_mask_locs)
         #L_g = self.link_pred_loss(learned_cell_embedding, mask_locs, target_gene_ids, target_rank_ids, edge_list)
         loss = L_mlm_geneonly + L_mlm_geneboth + L_mlm_rankonly + L_mlm_rankboth #+ L_g
-        gene_pp = self.pseudo_perp(predicted_gene_id, target_gene_ids, mask_locs)
-        rank_pp = self.pseudo_perp(predicted_rank_id, target_rank_ids, mask_locs)
-        subset = batch.dataset_name
+        gene_pp = self.pseudo_perp(predicted_gene_id, target_gene_ids, gene_mask_locs | both_mask_locs)
+        rank_pp = self.pseudo_perp(predicted_rank_id, target_rank_ids, rank_mask_locs | both_mask_locs)
+        subset = batch.dataset_name[0]
         self.log(f'{subset}_loss', loss)
-        self.log(f'{subset}_mlm_geneonly_loss', L_mlm_geneonly)
-        self.log(f'{subset}_mlm_geneboth_loss', L_mlm_geneboth)
-        self.log(f'{subset}_mlm_rankonly_loss', L_mlm_rankonly)
-        self.log(f'{subset}_mlm_rankboth_loss', L_mlm_rankboth)
+        self.log(f'{subset}_mlm_geneonly_loss', L_mlm_geneonly, batch_size=gene_mask_locs.sum())
+        self.log(f'{subset}_mlm_geneboth_loss', L_mlm_geneboth, batch_size=(gene_mask_locs|both_mask_locs).sum() )
+        self.log(f'{subset}_mlm_rankonly_loss', L_mlm_rankonly, batch_size=rank_mask_locs.sum())
+        self.log(f'{subset}_mlm_rankboth_loss', L_mlm_rankboth, batch_size=(rank_mask_locs|both_mask_locs).sum() )
         #self.log('train_link_pred_loss', L_g)
         self.log(f"{subset}_gene_perplexity", gene_pp, batch_size=1)
         self.log(f"{subset}_rank_perplexity", rank_pp, batch_size=1)
         return loss
         
     def training_step(self, batch, batch_idx):
-        loss = self._step(batch, batch_idx, subset="train")
+        loss = self._step(batch, batch_idx)
         return loss
         
 
-    def validation_step(self, batch, batch_idx):
-        loss = self._step(batch, batch_idx, subset="val")
+    def validation_step(self, batch, batch_idx, dataloader_idx):
+        loss = self._step(batch, batch_idx)
         return loss
 
-    def mask_nodes(self, tensor,mask_ratio=0.15):
-        """
-        Given a tensor, mask a ratio of the vectors in the tensor
-        This method of sampling allows for the same vector to be masked multiple times but also allows for the same vector to be masked multiple times
-        """
-        masked_tensor = tensor.clone()
-    
-        num_to_mask = int(tensor.size(0) * mask_ratio)
-        batch_indices = torch.randint(0, tensor.size(0), (num_to_mask,))
-        mask_value = torch.cat([
-            self.node_embedding(torch.tensor(MASK_IDX, device = tensor.device, dtype = torch.long)),
-            self.node_embedding(torch.tensor(MASK_IDX, device = tensor.device, dtype = torch.long))
-            ])
-        # for i in range(num_to_mask):
-        masked_tensor[batch_indices,  :] = mask_value
-        return masked_tensor, batch_indices
 
     def mlm_loss(self, predicted_gene_id, rank_global_gene_indices, mask_locs):
         