fix token masking

scGraphLLM/config.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 from _globals import *
 from copy import deepcopy
 import models as models
-import flash_transformer as flash_transformer
 #import linear_model as linear_model
 
 class Config(dict):
@@ -29,12 +28,7 @@ node_hyperparams = Config({
 
 transformer_dim = Config({
     "input_dim": 2*node_hyperparams.node_embedding_dim,
-    "feed_dim": 2*node_hyperparams.node_embedding_dim,
-    "hidden_dims": [2*node_hyperparams.node_embedding_dim, 
-                    2*node_hyperparams.node_embedding_dim, 
-                    2*node_hyperparams.node_embedding_dim],
-    "conv_dim": 2*node_hyperparams.node_embedding_dim, 
-    "out_dim": 2*node_hyperparams.node_embedding_dim,
+    "feed_dim": 256
 })
 
 vanilla_config = Config({
@@ -90,7 +84,7 @@ graph_kernel_attn_3L_config = Config({
     "num_heads": 8,
     "num_encoder_layers": 3,
     "activation": "gelu",
-    "dropout": 0.1,
+    "dropout": 0.5,
     "batch_first": True,
     "use_pe": False,
     "use_attn_mask": True,
@@ -110,7 +104,7 @@ graph_kernel_attn_6L_config = Config({
 })
 
 vanilla_manitou = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_config,
     "data_config":Config({
@@ -155,7 +149,7 @@ vanilla_manitou = Config({
 })
 
 vanilla_1024_base = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_config,
     "data_config":Config({
@@ -200,7 +194,7 @@ vanilla_1024_base = Config({
 })
 
 vanilla_1024 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_config,
     "data_config":Config({
@@ -245,7 +239,7 @@ vanilla_1024 = Config({
 })
 
 vanilla_2048 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_config,
     "data_config":Config({
@@ -290,7 +284,7 @@ vanilla_2048 = Config({
 })
 
 vanilla_4096 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_config,
     "data_config":Config({
@@ -335,7 +329,7 @@ vanilla_4096 = Config({
 })
 
 vanilla_3L_4096 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_3L_config,
     "data_config":Config({
@@ -380,7 +374,7 @@ vanilla_3L_4096 = Config({
 })
 
 vanilla_6L_4096 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM
+    "model": models.GDTransformer, # models.LitScGraphLLM
     "model_config": node_hyperparams, # prediction_head_config
     "transformer_config": vanilla_6L_config,
     "data_config":Config({
@@ -425,7 +419,7 @@ vanilla_6L_4096 = Config({
 })
 
 graph_kernel_attn_manitou = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_config,
     "data_config":Config({
@@ -470,7 +464,7 @@ graph_kernel_attn_manitou = Config({
 })
 
 graph_kernel_attn_1024_base = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_config,
     "data_config":Config({
@@ -515,7 +509,7 @@ graph_kernel_attn_1024_base = Config({
 })
 
 graph_kernel_attn_1024 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_config,
     "data_config":Config({
@@ -560,7 +554,7 @@ graph_kernel_attn_1024 = Config({
 })
 
 graph_kernel_attn_2048 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_config,
     "data_config":Config({
@@ -605,7 +599,7 @@ graph_kernel_attn_2048 = Config({
 })
 
 graph_kernel_attn_4096 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_config,
     "data_config":Config({
@@ -650,7 +644,7 @@ graph_kernel_attn_4096 = Config({
 })
 
 graph_kernel_attn_3L_4096 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_3L_config,
     "data_config":Config({
@@ -668,12 +662,12 @@ graph_kernel_attn_3L_4096 = Config({
     "trainer_config":Config({
         "max_epochs" : 100,
         "accelerator" : "gpu",
-        "max_time": "01:00:00:00",
+        "max_time": "05:00:00:00",
         "devices" : 1,
         "precision":"bf16",
         "num_sanity_val_steps" : 0,
         "log_every_n_steps" : 1,
-        "val_check_interval":0.05,
+        "val_check_interval":0.01,
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 5000 # Save every 5000 training steps
@@ -695,7 +689,7 @@ graph_kernel_attn_3L_4096 = Config({
 })
 
 graph_kernel_attn_6L_4096 = Config({
-    "model": flash_transformer.GDTransformer, # models.LitScGraphLLM,#
+    "model": models.GDTransformer, # models.LitScGraphLLM,#
     "model_config": node_hyperparams,
     "transformer_config": graph_kernel_attn_6L_config,
     "data_config":Config({

scGraphLLM/models.py
@@ -6,6 +6,7 @@ from MLP_modules import *
 import lightning.pytorch as pl
 from _globals import * ## these define the indices for the special tokens 
 from torch_geometric.utils import negative_sampling
+from transformer_modules import *
 
 class LitScGraphLLM(pl.LightningModule):
     def __init__(self, config):
@@ -88,7 +89,9 @@ class LitScGraphLLM(pl.LightningModule):
             masked_nodes_bool[masked_nodes] = True
             src_nodes = edge_index[0]
             dst_nodes = edge_index[1]
-            edge_mask = masked_nodes_bool[src_nodes] & masked_nodes_bool[dst_nodes]
+            # this line need to be changed to this for the "unmasked" data
+            #edge_mask = (masked_nodes_bool[src_nodes]) & (masked_nodes_bool[dst_nodes]) 
+            edge_mask = (~masked_nodes_bool[src_nodes]) & (~masked_nodes_bool[dst_nodes])
             pos_edge_index = edge_index[:, edge_mask]
             if pos_edge_index.size(1) == 0:
                 continue
@@ -148,3 +151,77 @@ class LitScGraphLLM(pl.LightningModule):
         optim_fn = self.optim_config["optimizer"]
         optimizer = optim_fn(self.parameters(), **self.optim_config.args)
         return optimizer
+
+
+
+class GDTransformer(LitScGraphLLM):
+    def __init__(self, config):
+        super().__init__(config)
+        self.tconfig = config.transformer_config
+        
+        if self.tconfig.num_encoder_layers == 1:
+            self.transformer_encoder = FlashTransformerEncoderLayer(
+                    self.tconfig.transformer_dim.input_dim, 
+                    self.tconfig.num_heads, 
+                    self.tconfig.transformer_dim.feed_dim, 
+                    self.tconfig.dropout, 
+                    self.tconfig.activation, 
+                    self.tconfig.batch_first,
+                    use_attn_mask=self.tconfig.use_attn_mask,
+                    use_PE=self.tconfig.use_pe,
+                    use_flash_attn=self.tconfig.use_flash_attn
+                )
+        else:
+            self.transformer_encoder = nn.ModuleList()
+            for i in range(self.tconfig.num_encoder_layers):
+                self.transformer_encoder.append(
+                    FlashTransformerEncoderLayer(
+                        self.tconfig.transformer_dim.input_dim, 
+                        self.tconfig.num_heads, 
+                        self.tconfig.transformer_dim.feed_dim, 
+                        self.tconfig.dropout, 
+                        self.tconfig.activation, 
+                        self.tconfig.batch_first,
+                        use_attn_mask=self.tconfig.use_attn_mask,
+                        use_PE=self.tconfig.use_pe,
+                        use_flash_attn=self.tconfig.use_flash_attn
+                    )   
+                )
+        self.node_embedding = torch.nn.Embedding(config.model_config.num_genes + config.model_config.num_ranks, 
+                                                 config.model_config.node_embedding_dim, padding_idx=PAD_IDX)
+        self.gene_prediction_head = RobertaLMHead(config.model_config.node_embedding_dim*2, config.model_config.num_genes)
+        self.rank_prediction_head = RobertaLMHead(config.model_config.node_embedding_dim*2, config.model_config.num_ranks)
+        self.optim_config = config.optim_config
+        self.loss_config = config.loss_config
+        self.use_attn_mask = self.tconfig.use_attn_mask
+        self.use_PE = self.tconfig.use_pe
+
+    def forward(self, batch):
+        orig_gene_id = batch["orig_gene_id"]
+        orig_rank_id = batch["orig_rank_indices"]
+        pe = batch["spectral_pe"].to(torch.float32) if self.use_PE else None # shape = (batch_size, seq_len, d_emb)
+        edge_index_list = batch["edge_index"]
+        num_nodes_list = batch["num_nodes"]
+        
+        # shape assertions for graph features
+        if self.use_PE:
+            assert pe.shape[1] == orig_gene_id.shape[1], f"Expect seqlen to be {orig_gene_id.shape[1]}, Got {pe.shape[1]}"
+        
+        mask_locs = [batch["gene_mask"], batch["rank_mask"], batch["both_mask"]]
+        
+        node_embedding = self.node_embedding(orig_gene_id) 
+        rank_embedding = self.node_embedding(orig_rank_id)
+        
+        combined_embedding = torch.concat([node_embedding, rank_embedding], dim=2)
+        
+        if self.tconfig.num_encoder_layers == 1:
+            combined_embedding = self.transformer_encoder(combined_embedding, p=pe, 
+                                                          edge_index_list=edge_index_list, 
+                                                          num_nodes_list=num_nodes_list)
+        else:
+            for encoder_layer in self.transformer_encoder:
+                combined_embedding = encoder_layer(combined_embedding, p=pe, 
+                                               edge_index_list=edge_index_list, 
+                                               num_nodes_list=num_nodes_list)
+        
+        return combined_embedding, orig_gene_id, orig_rank_id, mask_locs, edge_index_list, num_nodes_list