5a8d6d638797f538e490742ffba7d9a3e20ece2b
mingkz <mingxuan.zhang@columbia.edu>
2025-03-25 20:15:18 -0700

    add submission

--- submit_run.sh
+++ submit_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=Transformer_TRAIN
+#SBATCH --job-name=GDTransformer_PRETRAIN
 #SBATCH --output=./slurm_train_out/array_job_%A_%a.out
-#SBATCH --time=3-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --gpus=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --gpus=8
 #SBATCH -p gpu
 
 # Display all variables set by slurm
@@ -22,4 +22,4 @@ CONFIG_NAME=$2
 module load mamba
 mamba activate scllm_2
 
-python /home/mingxuan.zhang/scGraphLLM/scGraphLLM/run_training.py --config="$CONFIG_NAME" --mode="train" --name="$RUN_NAME"
+srun python /home/mingxuan.zhang/scGraphLLM/scGraphLLM/run_training.py --config="$CONFIG_NAME" --mode="train" --name="$RUN_NAME"
