Fixed expression-only masking and general cleanup

scGraphLLM/config.py
@@ -28,7 +28,7 @@ Node-specific hyperparameters
 -----------------------------
 """
 node_hyperparams = Config({
-    "num_ranks": NUM_RANKS, 
+    "num_expression_bins": NUM_EXPRESSION_BINS, 
     "num_genes": NUM_GENES, 
     "node_embedding_dim": 256,
     "num_hvgs": 4096,
@@ -162,7 +162,7 @@ graph_kernel_attn_4096_TEST = Config({
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps
@@ -211,7 +211,56 @@ graph_kernel_attn_4096 = Config({
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
+        "checkpoint_config": {
+                                "save_top_k": -1, # Save all checkpoints
+                                "every_n_train_steps" : 1000 # Save every 1000 training steps
+                            }
+    }),
+    "loss_config":Config({
+        "loss":"mlm"
+    }),
+    "optim_config":Config({
+        "optimizer": torch.optim.AdamW,
+        "args":{
+            "lr": 0.00005,
+            "betas": [0.9, 0.999],
+            "weight_decay": 0.0001,
+         }
+    }),
+    "repo_name":"scGraphLLM",
+    "wandb_user":"aqlab",
+    "wandb_project":"scGraphLLM",
+})
+
+
+graph_kernel_attn_3L_4096_sc = Config({
+    "model": models.GDTransformer,
+    "model_config": node_hyperparams,
+    "transformer_config": graph_kernel_attn_config,
+    "data_config":Config({
+        "train": Config({"cache_dir":"/hpc/projects/group.califano/GLM/data/single_cell/cxg_4096_NEW/train", "dataset_name": "train"}),  # NOTE: bc we are reading from disk each time, we need to cache in /pmglocal
+        "val": [
+            Config({"cache_dir":"/hpc/projects/group.califano/GLM/data/single_cell/cxg_4096_NEW/valSG", "dataset_name":"valSG"}),
+            Config({"cache_dir":"/hpc/projects/group.califano/GLM/data/single_cell/cxg_4096_NEW/valHOG", "dataset_name":"valHOG"})
+            ],
+        "test": [
+            ],
+        "run_test":False, 
+        "num_workers": 8,
+        "batch_size": 8
+    }),
+    "trainer_config":Config({
+        "max_epochs" : 10,
+        "accelerator" : "gpu",
+        "max_time": "07:00:00:00",
+        "devices" : 2,
+        "precision":"bf16",
+        "num_sanity_val_steps" : 0,
+        "log_every_n_steps" : 50,
+        "val_check_interval": 0.05,
+        "num_nodes":  1,
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps
@@ -260,7 +309,7 @@ graph_kernel_attn_1DIFF_4096 = Config({
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps
@@ -309,7 +358,7 @@ graph_kernel_attn_2DIFF_A_4096 = Config({
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps
@@ -358,7 +407,7 @@ graph_kernel_attn_2DIFF_B_4096 = Config({
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps
@@ -401,13 +450,13 @@ graph_kernel_attn_3L_4096 = Config({
         "max_epochs" : 10,
         "accelerator" : "gpu",
         "max_time": "07:00:00:00",
-        "devices" : 2,
+        "devices" : 8,
         "precision":"bf16",
         "num_sanity_val_steps" : 0,
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps
@@ -456,7 +505,7 @@ graph_kernel_attn_6L_4096 = Config({
         "log_every_n_steps" : 50,
         "val_check_interval": 0.05,
         "num_nodes":  1,
-        "strategy" :"ddp_find_unused_parameters_true",
+        "strategy" :"ddp", # "ddp_find_unused_parameters_true",
         "checkpoint_config": {
                                 "save_top_k": -1, # Save all checkpoints
                                 "every_n_train_steps" : 1000 # Save every 1000 training steps

scGraphLLM/data.py
@@ -73,6 +73,7 @@ def run_cache(
         if ncells % 1000 == 0:
             print(f"Processed {ncells} cells", end="\r")
 
+        # Get this cell/metacell's index number
         cell_number = expression.index[i]
         
         if msplit == "valSG":
@@ -84,12 +85,15 @@ def run_cache(
         else:
             split = msplit
         
+        # Path to which the file will be cached
         outfile = f"{cache_dir}/{split}/{cell_type}_{cell_number}.pt"
         if (os.path.exists(outfile)) and (not overwrite):
             ncells+=1
             continue
         
+        # Get this cell/metacell's expression
         cell: pd.Series = expression.iloc[i, :]
+        
         # filter out genes with 0 expression
         if only_expressed_genes:
             cell = cell[cell != ZERO_IDX]
@@ -103,7 +107,7 @@ def run_cache(
         if max_seq_length is not None and cell.shape[0] > max_seq_length:
             cell = cell.nlargest(n=max_seq_length)
 
-        # Subset network to only include genes in the cell
+        # Subset network to only include genes expressed in the cell
         network_cell = network[
             network[REG_VALS].isin(cell.index) & 
             network[TAR_VALS].isin(cell.index)
@@ -167,11 +171,7 @@ def cache_aracane_and_bins(
         skipped = 0
         ncells = 0
         
-        # msplit, ####
-        # min_genes_per_graph=MIN_GENES_PER_GRAPH, ####
-        # max_seq_length=None, ####
-        
-        for outdir_info in aracne_outdir_info: # If single==True, then this will only run once (for that single cell-type)
+        for outdir_info in aracne_outdir_info: # If single == True, then this will only run once (for that single cell-type)
             cell_type = outdir_info[0].split('/')[-2] # Get the cell-type's name that is currently being cached
             print(f"Caching: {cell_type}...")
             
@@ -211,46 +211,32 @@ class GraphTransformerDataset(torchDataset):
         return len(self.cached_files)
 
     def __getitem__(self, idx):
-        ## mask 5% as a gene only mask; mask 5% as a rank only mask ; mask 5% as both gene and rank mask
         data = torch.load(self.cached_files[idx], weights_only=False)
         node_indices = data.x
-        ## for each mask type, create boolean mask of the same shape as node_indices
+        # For each mask type, create boolean mask of the same shape as node_indices
         if self.mask_fraction == 0:
-            gene_mask = torch.zeros(node_indices.shape[0], dtype=torch.bool)
-            rank_mask = torch.zeros(node_indices.shape[0], dtype=torch.bool)
-            both_mask = torch.zeros(node_indices.shape[0], dtype=torch.bool)
+            expression_mask = torch.zeros(node_indices.shape[0], dtype=torch.bool)
         else:
-            gene_mask = torch.rand(node_indices.shape[0]) < self.mask_fraction
-            rank_mask = torch.rand(node_indices.shape[0]) < self.mask_fraction
-            both_mask = torch.rand(node_indices.shape[0]) < self.mask_fraction
+            expression_mask = torch.rand(node_indices.shape[0]) < self.mask_fraction
         
-        # mask the tensors
-        # node_indices[gene_mask, 0] = MASK_GENE_IDX
-        # node_indices[rank_mask, 1] = MASK_RANK_IDX
-        # node_indices[both_mask, :] = torch.tensor([MASK_GENE_IDX, MASK_RANK_IDX], dtype=node_indices.dtype)
-        
-        # add CLS
+        # Add CLS
         cls = torch.tensor([[CLS_GENE_IDX, CLS_TOKEN]], dtype=node_indices.dtype)
         node_indices = torch.cat([cls, node_indices], dim=0) # CLS can never be masked
 
-        # add False to masks
-        gene_mask = torch.cat([torch.tensor([False]), gene_mask])
-        rank_mask = torch.cat([torch.tensor([False]), rank_mask])
-        both_mask = torch.cat([torch.tensor([False]), both_mask])
+        # Add False to mask accounting for CLS
+        expression_mask = torch.cat([torch.tensor([False]), expression_mask])
 
         orig_gene_indices = node_indices[:, 0].clone()
-        orig_rank_indices = node_indices[:, 1].clone()
+        orig_expression_indices = node_indices[:, 1].clone()
         num_nodes = node_indices.shape[0] - 1 # discount the cls node
        
         # graph positional encoding
-        #spectral_pe = spectral_PE(edge_index=data.edge_index, num_nodes=node_indices.shape[0], k=64)
+        # spectral_pe = spectral_PE(edge_index=data.edge_index, num_nodes=node_indices.shape[0], k=64)
         
         return {
             "orig_gene_id" : orig_gene_indices, 
-            "orig_rank_indices" : orig_rank_indices, 
-            "gene_mask" : gene_mask, 
-            "rank_mask" : rank_mask, 
-            "both_mask" : both_mask,
+            "orig_expression_id" : orig_expression_indices, 
+            "expression_mask" : expression_mask, 
             "edge_index": data.edge_index,
             "num_nodes": num_nodes,
             #"spectral_pe": spectral_pe,

scGraphLLM/models.py
@@ -6,7 +6,7 @@ import lightning.pytorch as pl
 
 from scGraphLLM.GNN_modules import *
 from scGraphLLM.MLP_modules import *
-from scGraphLLM._globals import * ## these define the indices for the special tokens 
+from scGraphLLM._globals import * ## these define the id for the special tokens 
 from scGraphLLM.transformer_modules import *
 
 class LitScGraphLLM(pl.LightningModule):
@@ -18,12 +18,12 @@ class LitScGraphLLM(pl.LightningModule):
             padding_idx=PAD_GENE_IDX
         )
         
-        self.rank_embedding = torch.nn.Embedding(
-            num_embeddings=config.model_config.num_ranks, 
+        self.expression_embedding = torch.nn.Embedding(
+            num_embeddings=config.model_config.num_expression_bins, 
             embedding_dim=config.model_config.node_embedding_dim, 
-            padding_idx=PAD_RANK_IDX
+            padding_idx=PAD_EXPRESSION_IDX
         )
-        self.rank_prediction_head = RobertaLMHead(config.model_config.node_embedding_dim*2, config.model_config.num_ranks)
+        self.expression_prediction_head = RobertaLMHead(config.model_config.node_embedding_dim*2, config.model_config.num_expression_bins)
         self.optim_config = config.optim_config
         self.loss_config = config.loss_config
         
@@ -32,41 +32,41 @@ class LitScGraphLLM(pl.LightningModule):
     
     def _step(self, batch, batch_idx):
         learned_cell_embedding, target_gene_ids, target_expression_ids, mask_locs, edge_index_list, num_nodes_list = self(batch)
-        predicted_expression_id = self.rank_prediction_head(learned_cell_embedding)
-        gene_mask_locs, expression_mask_locs, rank_mask_locs = mask_locs
-        L_mlm_rankonly = self.mlm_loss(predicted_expression_id, target_expression_ids, rank_mask_locs)
-        loss = L_mlm_rankonly
-        rank_pp = self.pseudo_perp(predicted_expression_id, target_expression_ids, rank_mask_locs)
+        predicted_expression = self.expression_prediction_head(learned_cell_embedding)
+        expression_mask_locs = mask_locs
+        
+        L_mlm_expression = self.mlm_loss(predicted_expression, target_expression_ids, expression_mask_locs)
+        loss = L_mlm_expression
+        expression_perplexity = self.pseudo_perp(predicted_expression, target_expression_ids, expression_mask_locs)
+        
         if type(batch) == dict:
             subset = batch["dataset_name"][0]
         else:
             subset = batch.dataset_name[0]
 
-        self.log(f'{subset}_loss', loss, batch_size=1, add_dataloader_idx=False)
-        self.log(f'{subset}_mlm_rankonly_loss', L_mlm_rankonly, batch_size=expression_mask_locs.sum(), add_dataloader_idx=False)
-        self.log(f"{subset}_rank_perplexity", rank_pp, batch_size=1, add_dataloader_idx=False)
+        self.log(f'{subset}_loss', loss, batch_size=1, add_dataloader_idx=False, sync_dist=True) 
+        # self.log(f'{subset}_mlm_expression_loss', L_mlm_expression, batch_size=expression_mask_locs.sum(), add_dataloader_idx=False, sync_dist=True)
+        self.log(f"{subset}_expression_perplexity", expression_perplexity, batch_size=1, add_dataloader_idx=False, sync_dist=True)
         return loss
         
     def training_step(self, batch, batch_idx):
         loss = self._step(batch, batch_idx)
         return loss
         
-
     def validation_step(self, batch, batch_idx, dataloader_idx):
         loss = self._step(batch, batch_idx)
         return loss
 
-
-    def mlm_loss(self, predicted_gene_id, rank_global_gene_indices, mask_locs):
+    def mlm_loss(self, predicted_gene_id, expression_global_gene_id, mask_locs):
         masked_predictions = predicted_gene_id[mask_locs, :] ## because we record the location of the masked tokens, we cna retrieve just the masked tokens, and collapse the first dimension, ie mapping from n x r x G to m x G where m is the number of masked tokens
-        labels = rank_global_gene_indices[mask_locs]
+        labels = expression_global_gene_id[mask_locs]
         loss = F.cross_entropy(masked_predictions,labels)
         return loss
 
-    def pseudo_perp(self, predicted_gene_id, rank_global_gene_indices, mask_locs):
+    def pseudo_perp(self, predicted_gene_id, expression_global_gene_id, mask_locs):
         
         masked_predictions = predicted_gene_id[mask_locs, :] ## because we record the location of the masked tokens, we cna retrieve just the masked tokens, and collapse the first dimension, ie mapping from n x r x G to m x G where m is the number of masked tokens
-        labels = rank_global_gene_indices[mask_locs]
+        labels = expression_global_gene_id[mask_locs]
 
         # Get softmax probabilities
         sft = nn.Softmax(dim=1)
@@ -138,13 +138,13 @@ class GDTransformer(LitScGraphLLM):
             padding_idx=PAD_GENE_IDX
         )
         
-        self.rank_embedding = torch.nn.Embedding(
-            num_embeddings=config.model_config.num_ranks, 
+        self.expression_embedding = torch.nn.Embedding(
+            num_embeddings=config.model_config.num_expression_bins, 
             embedding_dim=config.model_config.node_embedding_dim, 
-            padding_idx=PAD_RANK_IDX
+            padding_idx=PAD_EXPRESSION_IDX
         )
         
-        self.rank_prediction_head = RobertaLMHead(config.model_config.node_embedding_dim*2, config.model_config.num_ranks)
+        self.expression_prediction_head = RobertaLMHead(config.model_config.node_embedding_dim*2, config.model_config.num_expression_bins)
         self.optim_config = config.optim_config
         self.loss_config = config.loss_config
         self.use_attn_mask = self.tconfig.use_attn_mask
@@ -152,30 +152,27 @@ class GDTransformer(LitScGraphLLM):
 
     def forward(self, batch):        
         orig_gene_id = batch["orig_gene_id"]
-        orig_rank_indices = batch["orig_rank_indices"]
-        mask = batch["rank_mask"] # SELECT MASK OPTION HERE
-        pe = batch["spectral_pe"].to(torch.float32) if self.use_PE else None # shape = (batch_size, seq_len, d_emb)
+        orig_expression_id = batch["orig_expression_id"]
+        mask = batch["expression_mask"]
         edge_index_list = batch["edge_index"]
         num_nodes_list = batch["num_nodes"]
+        pe = batch["spectral_pe"].to(torch.float32) if self.use_PE else None # shape = (batch_size, seq_len, d_emb)
         
         # IMPORTANT: Copy/clone the gene IDs and expression tensors, altering the originals will alter the training labels
         gene_ids = orig_gene_id.clone()
-        expression = orig_rank_indices.clone()
+        expression = orig_expression_id.clone() # Note above mainly applies to expression
         
         # Mask specified gene IDs and expression values
-        gene_ids[mask] = torch.tensor(MASK_GENE_IDX, dtype=gene_ids.dtype)
-        expression[mask] = torch.tensor(MASK_RANK_IDX, dtype=gene_ids.dtype)
+        expression[mask] = torch.tensor(MASK_EXPRESSION_IDX, dtype=gene_ids.dtype)
         
         # shape assertions for graph features
         if self.use_PE:
             assert pe.shape[1] == gene_ids.shape[1], f"Expect seqlen to be {gene_ids.shape[1]}, Got {pe.shape[1]}"
         
-        mask_locs = [batch["gene_mask"], batch["rank_mask"], batch["both_mask"]]
-        
         node_embedding = self.gene_embedding(gene_ids) 
-        rank_embedding = self.rank_embedding(expression)
+        expression_embedding = self.expression_embedding(expression)
         
-        combined_embedding = torch.concat([node_embedding, rank_embedding], dim=2)
+        combined_embedding = torch.concat([node_embedding, expression_embedding], dim=2)
         
         if self.tconfig.num_encoder_layers == 1:
             combined_embedding = self.transformer_encoder(combined_embedding, p=pe, 
@@ -191,8 +188,8 @@ class GDTransformer(LitScGraphLLM):
         del gene_ids
         del expression
         
-        # IMPORTANT: Make sure to return the correct orig_gene_id & orig_rank_indices as these are the un-altered, unmasked training labels (do not return gene_ids & expression)
-        return combined_embedding, orig_gene_id, orig_rank_indices, mask_locs, edge_index_list, num_nodes_list
+        # IMPORTANT: Make sure to return the correct orig_gene_id & orig_expression_id as these are the un-altered, unmasked training labels (do not return gene_ids & expression)
+        return combined_embedding, orig_gene_id, orig_expression_id, mask, edge_index_list, num_nodes_list
 
 
 # ------------------------------
@@ -219,7 +216,6 @@ class Perturb_GDTransformer(GDTransformer):
         num_nodes_list = batch["num_nodes"]
         pe = batch["spectral_pe"].to(torch.float32) if self.use_PE else None
         
-        # FIXME - WE SPLIT THE NODE_EMBEDDING INTO GENE/RANK_EMBEDDING
         ctrl_exp_embedding = self.node_embedding(x_c)
         
         if self.tconfig.num_encoder_layers == 1:
@@ -265,17 +261,17 @@ class Perturb_GDTransformer(GDTransformer):
     def training_step(self, batch, batch_idx):
         x_c, x_p, x_p_hat = self(batch)
         loss, mmd, ate_align = self.fine_tune_loss(x_c, x_p, x_p_hat)
-        self.log("train_loss", loss, batch_size=x_c.shape[0], add_dataloader_idx=False)
-        self.log("mmd_train", mmd, batch_size=x_c.shape[0], add_dataloader_idx=False)
-        self.log("ATE_align_train", ate_align, batch_size=x_c.shape[0], add_dataloader_idx=False)
+        self.log("train_loss", loss, batch_size=x_c.shape[0], add_dataloader_idx=False, sync_dist=True)
+        self.log("mmd_train", mmd, batch_size=x_c.shape[0], add_dataloader_idx=False, sync_dist=True)
+        self.log("ATE_align_train", ate_align, batch_size=x_c.shape[0], add_dataloader_idx=False, sync_dist=True)
         return loss
     
     def validation_step(self, batch, batch_idx):
         x_c, x_p, x_p_hat = self(batch)
         loss, mmd, ate_align = self.fine_tune_loss(x_c, x_p, x_p_hat)
-        self.log("val_loss", loss, batch_size=x_c.shape[0], add_dataloader_idx=False)
-        self.log("mmd_val", mmd, batch_size=x_c.shape[0], add_dataloader_idx=False)
-        self.log("ATE_align_val", ate_align, batch_size=x_c.shape[0], add_dataloader_idx=False)
+        self.log("val_loss", loss, batch_size=x_c.shape[0], add_dataloader_idx=False, sync_dist=True)
+        self.log("mmd_val", mmd, batch_size=x_c.shape[0], add_dataloader_idx=False, sync_dist=True)
+        self.log("ATE_align_val", ate_align, batch_size=x_c.shape[0], add_dataloader_idx=False, sync_dist=True)
         return loss
     
     def configure_optimizers(self):

scGraphLLM/run_training.py
@@ -26,14 +26,19 @@ def generate_random_string(length):
     return ''.join(random.choice(alphanumeric) for i in range(length))
 
 def collate_fn(batch):
-    data = { "orig_gene_id" : [], "orig_rank_indices" : [], "gene_mask" : [], 
-            "rank_mask" : [], "both_mask" : [], "edge_index": [], "num_nodes" :[], 
-             "dataset_name" : []}
+    data = { 
+            "orig_gene_id" : [], 
+            "orig_expression_id" : [], 
+            "expression_mask" : [], 
+            "edge_index": [], 
+            "num_nodes" :[], 
+            "dataset_name" : []
+    }
     
     # Make a dictionary of lists from the list of dictionaries
-    for b in batch:
+    for cell in batch:
         for key in data.keys():
-            data[key].append(b[key])
+            data[key].append(cell[key])
 
     # Pad these dictionaries of lists
     for key in data.keys():
@@ -41,9 +46,9 @@ def collate_fn(batch):
             continue
         elif key == "orig_gene_id":
             pad_value = PAD_GENE_IDX
-        elif key == "orig_rank_indices":
-            pad_value = PAD_RANK_IDX
-        elif key == "gene_mask" or key == "rank_mask" or key == "both_mask":
+        elif key == "orig_expression_id":
+            pad_value = PAD_EXPRESSION_IDX
+        elif key == "expression_mask":
             pad_value = False
         data[key] = pad_sequence(data[key], batch_first=True, padding_value=pad_value)
 
@@ -64,6 +69,7 @@ parser.add_argument('--config', type=str, help='path to model config file',requi
 parser.add_argument('--version', type=str, help='run version', default=None)
 parser.add_argument('--name', type=str, help='run name', default=None)
 parser.add_argument('--mode', type=str, help=' valid modes: [train, resume, debug, predict, validate]', default=None, required=True)
+parser.add_argument('--devices', type=int, help='number of GPUs', default=None)
 parser.add_argument('--ckpt-file', type=str, help='name of checkpoint file only, no paths', default=None)
 parser.add_argument('--override-config', type=str, help='wandb sweep style cl args that will be parsed and will update config accordingly', default=None)
 
@@ -98,6 +104,10 @@ def main(args):
         if args.version is None or args.ckpt_file is None:
             raise ValueError("Must specify version and ckpt file for resume, predict, or validate mode")
         
+    devices = args.devices # This is an optional argument if you want to overwrite the number of devices specified in config.py
+    if devices != None: # Override the number of devices/GPUs
+            mconfig['trainer_config']['devices']=devices
+    
     version = args.version
     if version is None:
         ## this does not break for ddp processes 
@@ -200,7 +210,6 @@ def main(args):
         raise NotImplementedError(f"mode {mode} not implemented")
 
     ##  copy checkpoints to shared dir and clean up only on 1 gpu 
-
     if ((mconfig['trainer_config']['devices'] == 1) or (torch.distributed.get_rank() == 0)) and (mode != "debug") :
         if copy_checkpoints:
             time.sleep(30) ## potentially wait for other processes to finish writing to disk