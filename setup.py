f0a9e0a792ac696a104507d04f624e161eb0c41c
rocassius <rowancassius@gmail.com>
2025-06-16 09:53:20 -0700

    update setup py with requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name="scGraphLLM",
-    version="0.0.1",
-    packages=find_packages(include=["scGraphLLM"])
+    version="1.0.0",
+    packages=find_packages(include=["scGraphLLM"]),
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy==1.26.0",
+        "scipy",
+        "pandas",
+        "scikit-learn",
+        "polars",
+        "pyarrow",
+        "ipykernel",
+        "wandb",
+        "jupyter",
+        "loralib",
+        "viper-in-python",
+        "scanpy",
+        "python-igraph",
+        "leidenalg",
+        "louvain",
+        "lightning"
+    ]
 )
